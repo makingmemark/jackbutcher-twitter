0dc2ead7d5a3b2a1bd4c6074ac7bf49be0344cb9
Mark Lowe <mark@liquid.com.au>
2023-07-27 10:42:16 +1000

    cygaars original edited

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## About
 
-Don't like the Twitter rebrand to x.com? Neither did I, so I made this Chrome extension that lets you revert x-branding back to the beloved bird.
+Don't like the Twitter rebrand to x.com? Neither did I, so I made this Chrome extension that lets you r̶e̶v̶e̶r̶t̶ ̶x̶-̶b̶r̶a̶n̶d̶i̶n̶g̶ ̶b̶a̶c̶k̶ ̶t̶o̶ ̶t̶h̶e̶ ̶b̶e̶l̶o̶v̶e̶d̶ ̶b̶i̶r̶d̶ set the x branding to Jack Butcher's concept.
+
+Shout out to Cygaar for creating the original "old twitter" extension.
+
+[Cygaar's "old twitter" repo](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
 
 The loading icon still needs to be replaced.
 
--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,5 @@
 // Set favicon
-document.querySelector("link[rel~='icon']").href = chrome.runtime.getURL("../assets/twitter-favicon.png");
+document.querySelector("link[rel~='icon']").href = chrome.runtime.getURL("../assets/twitter-jackbutcher-favicon.png");
 
 function waitForElement(selector) {
   return new Promise(resolve => {
@@ -33,7 +33,7 @@ waitForElement('[aria-label="Twitter"]').then((elm) => {
   container.innerHTML = "";
 
   const twitterIcon = document.createElement("img");
-  twitterIcon.src = chrome.runtime.getURL("../assets/twitter.png");
+  twitterIcon.src = chrome.runtime.getURL("../assets/twitter-jackbutcher.png");
   twitterIcon.width = 42;
   twitterIcon.height = 42;
   container.appendChild(twitterIcon);
